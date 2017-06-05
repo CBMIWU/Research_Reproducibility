cbe4a1795d6e47d246b400cf7de5188b9e67e6f6
Langston Barrett <langston.barrett@gmail.com>
2017-06-05 21:39:22 +0000

    default.nix: init

---
+++ RepeatAutomator/default.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  # Pin a nixpkgs version
+  pinned_pkgs = import (pkgs.fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+    rev    = "17.03";
+    sha256 = "1fw9ryrz1qzbaxnjqqf91yxk1pb9hgci0z0pzw53f675almmv9q2";
+  }) {};
+in with pinned_pkgs; python2Packages.buildPythonApplication rec {
+  name = "repeat-automator";
+
+  src = "./.";
+
+  nativeBuildInputs = [
+    python2Packages.setuptools # not currently used, but should be
+  ];
+
+  propagatedBuildInputs = with python2Packages; [
+    beautifulsoup
+    pycurl
+  ];
+
+  meta = with stdenv.lib; {
+    homepage = https://github.com/CBMIWU/Research_Reproducibility;
+    description = "A tool for analyzing the reproducibility of EMR studies";
+    license = stdenv.lib.licenses.apache2;
+    platforms = stdenv.lib.platforms.unix;
+    maintainers = [ stdenv.lib.maintainers.siddharthist ];
+  };
+}
